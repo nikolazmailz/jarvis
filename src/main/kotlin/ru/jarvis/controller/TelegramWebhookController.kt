a5793647653b4648e1e165231e4966866681895d
nikolazmailz <nikolazmailz@gmail.com>
2025-11-19 14:38:49 +0300

    Adjust Telegram webhook controller per review

---
+++ src/main/kotlin/ru/jarvis/controller/TelegramWebhookController.kt
@@ -0,0 +1,26 @@
+package ru.jarvis.controller
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import ru.jarvis.domain.telegram.TelegramWebhookRequest
+
+/**
+ * Контроллер, принимающий callback-запросы Telegram webhook.
+ */
+@RestController
+@RequestMapping("/tg")
+class TelegramWebhookController {
+
+    private val log = KotlinLogging.logger {}
+
+    @PostMapping("/webhook")
+    fun handleWebhook(@RequestBody request: TelegramWebhookRequest): ResponseEntity<Unit> {
+        log.info { "Received Telegram update: $request" }
+        // TODO: delegate processing to application service once it appears.
+        return ResponseEntity.ok().build()
+    }
+}
